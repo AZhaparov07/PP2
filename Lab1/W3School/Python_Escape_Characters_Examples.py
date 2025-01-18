207a88790b2b947c69f7df143d119d2e0f8ea5d6
Arystanbek <arystanbekzhaparov07@gmail.com>
2025-01-18 22:05:36 +0500

    W3School Example Lab1 pt2

---
+++ Lab1/W3School/Python_Escape_Characters_Examples.py
@@ -0,0 +1,16 @@
+#Example 1 (Incorrect)
+txt = "We are the so-called "Vikings" from the north."
+
+#Example 2
+txt = "We are the so-called \"Vikings\" from the north."
+
+#Examples 3
+#\'	#Single Quote	
+#\\	#Backslash	
+#\n	#New Line	
+#r	Carriage Return	
+#\t	Tab	
+#\b	Backspace	
+#\f	Form Feed	
+#\ooo	Octal value	
+#\xhh	Hex value
---
+++ Lab1/W3School/Python_Format_Strings_Examples.py
@@ -0,0 +1,23 @@
+#Example 1
+age = 36
+txt = "My name is John, I am " + age
+print(txt)
+
+#Example 2
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+#Example 3
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+#Example 4
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+#Example 5
+txt = f"The price is {20 * 59} dollars"
+print(txt)
